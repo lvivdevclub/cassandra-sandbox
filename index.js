d2f04f6986949bd07e157913b9da077779d05254
Ihor Banadiga <banadiga@gmail.com>
2020-09-04 11:25:28 +0300

    up dse-server version

--- index.js
+++ index.js
@@ -2,11 +2,12 @@ const cassandra = require('cassandra-driver');
 
 const client = new cassandra.Client({
     contactPoints: ['node1', 'node2', 'node3'],
-    // localDataCenter: 'datacenter1',
+    localDataCenter: 'DC1',
     keyspace: 'ks1'
 });
 
-const query = 'SELECT name, email FROM users WHERE key = ?';
+const query = 'SELECT name, email FROM users WHERE email = ?';
 
-client.execute(query, [ 'someone' ])
-    .then(result => console.log('User with email %s', result.rows[0].email));
+client.execute(query, [ 'email1' ])
+    .then(result => console.log('User with name %s', result.rows[0].name))
+    .catch(error => console.error('error: ', error));
